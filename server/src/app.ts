334a158de9d037170023fb182da4db15ac4f7b74
Krum <krumantonov@gmail.com>
2024-01-17 13:14:39 +0200

    added vue js client

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log("Server is running on port 8080");
+server.listen(8081, () => {
+    console.log("Server is running on port 8081");
 });
 
 connectDB();
