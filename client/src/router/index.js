f2cea5241e540210768d0deadc4e9a6508bf6a01
Krum <krumantonov@gmail.com>
2024-01-17 18:15:22 +0200

    Added login page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,21 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
+import LandingPage from '@/components/LandingPage'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'LandingPage',
+      component: LandingPage
     },
     {
       path: '/auth/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/auth/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
