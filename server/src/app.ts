9e4332e9b5f9226b1b6ab9f9f1fd02240a51ae08
Krum <krumantonov@gmail.com>
2024-02-02 13:52:17 +0200

    added endoint to get historical tvl for a protocol

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import compression from "compression";
 import cors from "cors";
 import { connectDB } from "./config/db";
 import router from "./router";
+import axios from 'axios';
 
 const app = express();
 
@@ -27,3 +28,29 @@ app.use(compression());
 app.use(cookieparser());
 
 app.use('/', router());
+
+
+
+const coingeckoApiKey = 'CG-G3pobU3E8Q1w1wms5w9x8AVu';
+
+// Defillama API URLs
+const defillamaBaseURL = 'https://api.llama.fi';
+
+// CoinGecko API URLs
+const coingeckoBaseURL = 'https://api.coingecko.com/api/v3';
+
+// Defillama API Calls
+app.get('/defillama-tvl/:protocol', async (req, res) => {
+  try {
+    const protocolName = req.params.protocol;
+
+    const response = await axios.get(`${defillamaBaseURL}/protocol/${protocolName}`);
+
+    const totalTvl = response.data.tvl || [];
+
+    res.json(totalTvl);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
