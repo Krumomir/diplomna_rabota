5e3aa4f670d886e1cc13394cf94f4b4575f35ce3
Krum <krumantonov@gmail.com>
2024-02-07 13:54:52 +0200

    added endpoint to fetch coins data

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import { connectDB } from "./config/db";
 import router from "./router";
 import axios from 'axios';
+import { processResponse } from './helpers';
 
 const app = express();
 
@@ -30,16 +31,13 @@ app.use(cookieparser());
 app.use('/', router());
 
 
-
 const coingeckoApiKey = 'CG-G3pobU3E8Q1w1wms5w9x8AVu';
 
-// Defillama API URLs
 const defillamaBaseURL = 'https://api.llama.fi';
 
-// CoinGecko API URLs
 const coingeckoBaseURL = 'https://api.coingecko.com/api/v3';
 
-// Defillama API Calls
+//TVL: Retrieve current and historical TVL data for Aave
 app.get('/defillama-tvl/:protocol', async (req, res) => {
   try {
     const protocolName = req.params.protocol;
@@ -54,3 +52,71 @@ app.get('/defillama-tvl/:protocol', async (req, res) => {
   }
 });
 
+
+app.get('/defillama-current-tvl/:protocol', async (req, res) => {
+  try {
+    const protocolName = req.params.protocol;
+
+    const response = await axios.get(`${defillamaBaseURL}/tvl/${protocolName}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// app.get('/defillama-yields/:protocol', async (req, res) => {
+//   try {
+//     // Extract protocol name from request parameters
+//     const protocolName = req.params.protocol;
+
+//     // Yields API Call for the specified protocol
+//     const response = await axios.get(`${defillamaBaseURL}/pools/${protocolName}`);
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+// Endpoint to fetch coin data
+app.get('/coin/:id', async (req: express.Request, res: express.Response) => {
+  try {
+    const coinId = req.params.id;
+    const { data } = await axios.get(`${coingeckoBaseURL}/coins/${coinId}`, {
+      params: {
+        localization: false,
+        tickers: false,
+        market_data: true, // Set market_data to true
+        community_data: false,
+        developer_data: false,
+        sparkline: false,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CoinGecko-API-Key': coingeckoApiKey, // Include your API key in the request headers
+      },
+    });
+    
+    const processedResponse = processResponse(data);
+
+    res.json(processedResponse);
+  } catch (error) {
+    console.error('Error fetching coin data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+// app.get('/coingecko-exchanges', async (_, res) => {
+//   try {
+//     // Exchanges API Call for Aave on CoinGecko with API key
+//     const response = await axios.get(`${coingeckoBaseURL}/coins/aave/tickers`, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-CoinGecko-API-Key': coingeckoApiKey,
+//       },
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
--- server/src/helpers/index.ts
+++ server/src/helpers/index.ts
@@ -6,3 +6,60 @@ export const random = () => crypto.randomBytes(128).toString('base64');
 export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
 };
+
+export const processResponse = (response: any) => {
+    // Extracting only the USD values
+    const usdValues = {
+        price_change_percentage_1h: response.market_data.price_change_percentage_1h_in_currency.usd,
+        price_change_percentage_24h: response.market_data.price_change_percentage_24h_in_currency.usd,
+        price_change_percentage_7d: response.market_data.price_change_percentage_7d_in_currency.usd,
+        price_change_percentage_14d: response.market_data.price_change_percentage_14d_in_currency.usd,
+        price_change_percentage_30d: response.market_data.price_change_percentage_30d_in_currency.usd,
+        price_change_percentage_60d: response.market_data.price_change_percentage_60d_in_currency.usd,
+        price_change_percentage_200d: response.market_data.price_change_percentage_200d_in_currency.usd,
+        price_change_percentage_1y: response.market_data.price_change_percentage_1y_in_currency.usd,
+        market_cap_change_24h: response.market_data.market_cap_change_24h_in_currency.usd,
+        market_cap_change_percentage_24h: response.market_data.market_cap_change_percentage_24h_in_currency.usd,
+        ath: response.market_data.ath.usd,
+        ath_change_percentage: response.market_data.ath_change_percentage.usd,
+        atl: response.market_data.atl.usd,
+        atl_change_percentage: response.market_data.atl_change_percentage.usd,
+        market_cap: response.market_data.market_cap.usd,
+        fully_diluted_valuation: response.market_data.fully_diluted_valuation.usd,
+        total_volume: response.market_data.total_volume.usd,
+        high_24h: response.market_data.high_24h.usd,
+        low_24h: response.market_data.low_24h.usd,
+        current_price: response.market_data.current_price.usd,  
+        ath_date: response.market_data.ath_date.usd,
+        atl_date: response.market_data.atl_date.usd,
+        price_change_24h_in_currency: response.market_data.price_change_24h_in_currency.usd,
+        price_change_percentage_1h_in_currency: response.market_data.price_change_percentage_1h_in_currency.usd,
+        price_change_percentage_24h_in_currency: response.market_data.price_change_percentage_24h_in_currency.usd,
+        price_change_percentage_7d_in_currency: response.market_data.price_change_percentage_7d_in_currency.usd,
+        price_change_percentage_14d_in_currency: response.market_data.price_change_percentage_14d_in_currency.usd,
+        price_change_percentage_30d_in_currency: response.market_data.price_change_percentage_30d_in_currency.usd,
+        price_change_percentage_60d_in_currency: response.market_data.price_change_percentage_60d_in_currency.usd,
+        price_change_percentage_200d_in_currency: response.market_data.price_change_percentage_200d_in_currency.usd,
+        price_change_percentage_1y_in_currency: response.market_data.price_change_percentage_1y_in_currency.usd,
+        market_cap_change_24h_in_currency: response.market_data.market_cap_change_24h_in_currency.usd,
+        market_cap_change_percentage_24h_in_currency: response.market_data.market_cap_change_percentage_24h_in_currency.usd
+    };
+
+    delete response.image;
+    delete response.links.blockchain_site;
+    delete response.links.official_forum_url;
+    delete response.links.chat_url;
+    delete response.links.announcement_url;
+    delete response.categories;
+    delete response.detail_platforms;
+
+    // Update the fields within the market_data object with the extracted USD values
+    response.market_data = {
+        ...response.market_data,
+        ...usdValues
+    };
+
+    // Return the modified response
+    return response;
+};
+  
